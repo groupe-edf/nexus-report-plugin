c880fe007fb673f3e5cf38f23ab353e64451dd96
Mathieu Delrocq <delrocq.mathieu@gmail.com>
2020-07-24 16:44:21 +0200

    config of the front with react

---
+++ webpack.common.js
@@ -0,0 +1,85 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+const CopyModulesPlugin = require('copy-modules-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const path = require('path');
+const libImgDir = path.resolve(__dirname, 'node_modules/@sonatype/react-shared-components/assets/img');
+
+module.exports = {
+  entry: './src/frontend/src',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        include: libImgDir,
+        loader: 'file-loader',
+        options: {
+          name: 'img/[name].[ext]'
+        }
+      },
+      {
+        test: /\.(ttf|eot|woff2?|svg)$/,
+        exclude: libImgDir,
+        loader: 'file-loader',
+        options: {
+          name: 'fonts/[name].[ext]'
+        }
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [
+        new TerserJSPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: true
+        }),
+        new OptimizeCSSAssetsPlugin({})]
+  },
+  plugins: [
+    new CopyModulesPlugin({
+      destination: path.resolve(__dirname, 'target/webpack-modules')
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'nexus-report-bundle.css'
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  externals: {
+    react: 'react',
+    axios: 'axios',
+    xstate: 'xstate'
+  }
+};
