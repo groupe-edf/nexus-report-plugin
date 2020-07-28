ca184ffe69b11a90bdc78ff60ba75bfd7f97e268
Mathieu Delrocq <delrocq.mathieu@gmail.com>
2020-07-28 14:56:40 +0200

    add report form DTO

---
+++ src/main/java/org/sonatype/nexus/plugins/report/internal/ReportXO.java
@@ -0,0 +1,23 @@
+package org.sonatype.nexus.plugins.report.internal;
+
+public class ReportXO {
+    String fileName;
+    String repositoryName;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getRepositoryName() {
+        return repositoryName;
+    }
+
+    public void setRepositoryName(String repositoryName) {
+        this.repositoryName = repositoryName;
+    }
+
+}
