cb45e23d153bd9de84ba4b0c7cf6276f556e8247
Mathieu Delrocq <delrocq.mathieu@gmail.com>
2020-08-21 17:07:56 +0200

    build Excel file

--- src/main/java/org/sonatype/nexus/plugins/report/internal/builders/ExcelReportBuilder.java
+++ src/main/java/org/sonatype/nexus/plugins/report/internal/builders/ExcelReportBuilder.java
@@ -5,19 +5,18 @@ import java.util.List;
 
 import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.apache.poi.ss.SpreadsheetVersion;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.DataFormat;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.AreaReference;
 import org.apache.poi.ss.util.CellReference;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFTable;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTTable;
-import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTTableColumn;
-import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTTableColumns;
-import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTTableStyleInfo;
-import org.sonatype.nexus.repository.storage.Component;
 
 public class ExcelReportBuilder {
 
@@ -35,45 +34,52 @@ public class ExcelReportBuilder {
 
     public Sheet buildSheet(String sheetName, List<ComponentInfos> componentsInfos) {
         XSSFSheet sheet = (XSSFSheet) workbook.createSheet(sheetName);
+        DataFormat format = workbook.createDataFormat();
+        CellStyle style = workbook.createCellStyle();
+        style.setDataFormat(format.getFormat("#,##0.00"));
         final List<String> headers = ComponentInfos.getComponentInfosTitles();
 
-        XSSFTable table = sheet.createTable(new AreaReference(new CellReference(0, 0),
-                new CellReference(componentsInfos.size() - 1, headers.size() - 1), SpreadsheetVersion.EXCEL2007));
-        CTTable cttable = table.getCTTable();
-        CTTableStyleInfo cttableStyle = cttable.addNewTableStyleInfo();
-        cttableStyle.setName("TableStyleMedium13");
-        CTTableColumns columns = cttable.addNewTableColumns();
-        columns.setCount(headers.size());
-        for (int i = 0; i < headers.size(); i++) {
-            CTTableColumn column = columns.addNewTableColumn();
-            column.setName(headers.get(i));
-            column.setId(i + 1);
+        int rowIndex = 0;
+        // Header row
+        XSSFRow headerRow = sheet.createRow(rowIndex);
+        rowIndex++;
+        for (int colIndex = 0; colIndex < headers.size(); colIndex++) {
+            headerRow.createCell(colIndex).setCellValue(headers.get(colIndex));
         }
-        int rownum = 0;
+        // Datas rows
         for (int i = 0; i < componentsInfos.size(); i++) {
-            XSSFRow row = sheet.createRow(i);
-            if (rownum == 0) {
-                for (int j = 0; j < headers.size(); j++) {
-                    row.createCell(j).setCellValue(headers.get(j));
-                }
-            } else {
-                row.createCell(0).setCellValue(componentsInfos.get(i).getGroup());
-                row.createCell(1).setCellValue(componentsInfos.get(i).getName());
-                row.createCell(2).setCellValue(componentsInfos.get(i).getVersion());
-                row.createCell(3).setCellValue(componentsInfos.get(i).getFormat());
-                row.createCell(4).setCellValue(componentsInfos.get(i).getSize());
-                row.createCell(5).setCellValue(componentsInfos.get(i).getSizeMo());
-                row.createCell(6).setCellValue(componentsInfos.get(i).getSizeGo());
-                row.createCell(7).setCellValue(componentsInfos.get(i).getCreatedBy());
-                row.createCell(8).setCellValue(componentsInfos.get(i).getLastUpdated());
-                row.createCell(9).setCellValue(componentsInfos.get(i).getLastDownloaded());
-                row.createCell(10).setCellValue(componentsInfos.get(i).getEncoded());
-            }
-            rownum++;
+            XSSFRow row = sheet.createRow(rowIndex);
+            rowIndex++;
+            row.createCell(0).setCellValue(componentsInfos.get(i).getGroup());
+            row.createCell(1).setCellValue(componentsInfos.get(i).getName());
+            row.createCell(2).setCellValue(componentsInfos.get(i).getVersion());
+            row.createCell(3).setCellValue(componentsInfos.get(i).getFormat());
+            row.createCell(4).setCellValue(componentsInfos.get(i).getSize());
+            XSSFCell cell = row.createCell(5);
+            cell.setCellValue(componentsInfos.get(i).getSizeMo());
+            cell.setCellStyle(style);
+            cell = row.createCell(6);
+            cell.setCellValue(componentsInfos.get(i).getSizeGo());
+            cell.setCellStyle(style);
+            row.createCell(7).setCellValue(componentsInfos.get(i).getCreatedBy());
+            row.createCell(8).setCellValue(componentsInfos.get(i).getLastUpdated());
+            row.createCell(9).setCellValue(componentsInfos.get(i).getLastDownloaded());
+            row.createCell(10).setCellValue(componentsInfos.get(i).getEncoded());
         }
-        cttable.addNewAutoFilter();
-
+        // AutoSize Column
+        XSSFRow firstRow = sheet.getRow(sheet.getFirstRowNum());
+        for (int colIndex = 0; colIndex < headers.size(); colIndex++) {
+            sheet.autoSizeColumn(colIndex);
+        }
+        // Apply table style
+        if (!componentsInfos.isEmpty()) {
+            XSSFTable table = sheet.createTable(new AreaReference(new CellReference(0, 0),
+                    new CellReference(sheet.getLastRowNum(), firstRow.getLastCellNum() - 1),
+                    SpreadsheetVersion.EXCEL2007));
+            table.setStyleName("TableStyleMedium13");
+            CTTable cttable = table.getCTTable();
+            cttable.addNewAutoFilter();
+        }
         return sheet;
     }
-
 }
